04a9c2a6532810b97c71af5e8ef4657c131f8216
Rafaela <rafaelacardosoteixeira907@gmail.com>
2022-12-01 16:34:32 -0300

    atributos Pessoa para privado

--- Rede_Social/src/Pessoa.java
+++ Rede_Social/src/Pessoa.java
@@ -1,11 +1,55 @@
 public abstract class Pessoa {
 
-    String nome;
-    int idade;
-    String email;
-    String Biografia;
-    String genero;
+   private String nome;
+   private int idade;
+   private String email;
+   private String Biografia;
+   private String genero;
 
+
+   public abstract Mm_idade(int );
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBiografia() {
+        return this.Biografia;
+    }
+
+    public void setBiografia(String Biografia) {
+        this.Biografia = Biografia;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
 
 }
--- Rede_Social/src/Usuario.java
+++ Rede_Social/src/Usuario.java
@@ -1,3 +1,14 @@
-public class Usuario extends Pessoa{
+public class Usuario extends Pessoa {
     
+    public void Mm_idade(int idade2){
+        if (getIdade() < 18){
+
+            System.out.println
+
+
+        }
+
+
+    }
+
 }
