59fa0658a3917dc0c3c9a14e27091e477a78a457
AYELE SENA LIMA <05387797135@ifto.local>
2022-12-01 17:01:27 -0300

    criei o chat

---
+++ Rede_Social/src/chat.java
@@ -0,0 +1,40 @@
+public class chat {
+
+    private int qtd_contatos;
+    private int qtd_de_msg_arq;
+    private int qtd_msg;
+    private int qtd_notificacao;
+
+    public int getQtd_contatdos() {
+        return this.qtd_contatos;
+    }
+
+    public void setQtd_contatos(int qtd_contatos) {
+        this.qtd_contatos = qtd_contatos;
+    }
+
+    public int getQtd_de_msg_arq() {
+        return this.qtd_de_msg_arq;
+    }
+
+    public void setQtd_de_msg_arq(int qtd_contatos) {
+        this.qtd_de_msg_arq = qtd_contatos;
+    }
+
+    public int getQtd_msg() {
+        return this.qtd_msg;
+    }
+
+    public void setQtd_msg(int qtd_msg) {
+        this.qtd_de_msg_arq = qtd_msg;
+    }
+    
+    public int getQtd_notificacao() {
+        return this.qtd_notificacao;
+    }
+
+    public void setQtd_notificacao(int qtd_notificacao) {
+        this.qtd_notificacao = qtd_notificacao;
+    }
+
+}
