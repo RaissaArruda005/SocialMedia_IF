969a66f9ce806242553004679d99ab00650c5571
Rafaela <rafaelacardosoteixeira907@gmail.com>
2022-12-08 15:11:38 -0300

    Estou inplementando o método cadastro

--- Rede_Social/src/App.java
+++ Rede_Social/src/App.java
@@ -10,15 +10,28 @@ public class App {
         int valor;
         String nome;
         int escolha = 0;
-        
 
-        //System.out.println("Informe sua idade: ");
-        //valor = sc.nextInt();
-        //usu.controleIdade(valor);
+
+        System.out.println("Informe sua idade: ");
+        valor = sc.nextInt();
+        usu.controleIdade(valor);
+
+        if (valor >= 18){
 
         System.out.println("Informe seu nome: ");
         nome = sc.nextLine();
         usu.cadastrar(nome);
+        }
+        
+        else{
+            System.out.println("Não pode fazer cadastro");
+
+
+        }
+
+
+
+        
 
         
 
